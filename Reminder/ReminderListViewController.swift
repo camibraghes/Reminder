c719c1ea613e835958c5f94dcb58333682df052e
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-29 15:17:48 +0200

    Configure the data source

--- Reminder/ReminderListViewController.swift
+++ Reminder/ReminderListViewController.swift
@@ -1,11 +1,17 @@
 import UIKit
 
-class ReminderListViewController: UICollectionViewController {
-
+class ReminderListViewController: UICollectionViewController {    
     override func viewDidLoad() {
         super.viewDidLoad()
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
     }
     
     func listLayout() -> UICollectionViewLayout {
